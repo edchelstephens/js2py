2746d7d0612a1d8093929296d5113f869d4a072a
Edchel Stephen <edchelstephens@gmail.com>
2022-02-25 06:33:00 +0800

    browser tests - first passing test.

--- tests/browser_tests/test_site_functionality.py
+++ tests/browser_tests/test_site_functionality.py
@@ -4,4 +4,4 @@ firefox_path = "/home/ed/.drivers/geckodriver"
 browser = webdriver.Firefox(executable_path=firefox_path)
 browser.get("http://localhost:8000")
 
-assert "Django" in browser.title
+assert "worked" in browser.title
