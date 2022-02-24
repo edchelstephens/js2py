80e01b0edbab67df45940c8f4bfaf50e0c86f59b
Edchel Stephen <edchelstephens@gmail.com>
2022-02-25 06:44:15 +0800

    make url render index.html of ui.

--- js2py/urls.py
+++ js2py/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+
+from django.urls import re_path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    re_path(".*", TemplateView.as_view(template_name="ui/index.html")),
 ]
