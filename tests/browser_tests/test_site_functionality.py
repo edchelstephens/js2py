cf4c93f7224251591e3b2396e533f6f735c09cf0
Edchel Stephen <edchelstephens@gmail.com>
2022-02-25 06:39:08 +0800

    update browser test.

--- tests/browser_tests/test_site_functionality.py
+++ tests/browser_tests/test_site_functionality.py
@@ -4,4 +4,4 @@ firefox_path = "/home/ed/.drivers/geckodriver"
 browser = webdriver.Firefox(executable_path=firefox_path)
 browser.get("http://localhost:8000")
 
-assert "worked" in browser.title
+assert "Js2Py" in browser.title
