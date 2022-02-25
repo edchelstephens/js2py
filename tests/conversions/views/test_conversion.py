e138e0a98ef17a274def9655b8e553dc2426f54f
Edchel Stephen <edchelstephens@gmail.com>
2022-02-25 11:39:15 +0800

    test views.

---
+++ tests/conversions/views/test_conversion.py
@@ -0,0 +1,35 @@
+import json
+import pprint
+
+from rest_framework.test import APITestCase
+from rest_framework.test import APIRequestFactory
+
+from conversions.views.conversion import ConversionAPIView
+
+
+class ConversionAPIViewTestCase(APITestCase):
+    """ConversionAPIView test case."""
+
+    def setUp(self) -> None:
+        self.request_factory = APIRequestFactory()
+        self.view = ConversionAPIView.as_view()
+        self.url = "conversions/js-to-python/"
+
+    def test_post_successfully_converts_js_object_to_python(self) -> None:
+        """Post successfully converts js object to python."""
+
+        input_object = "1"
+        data = {"data": input_object}
+        request = self.request_factory.post(self.url, data=data)
+        response = self.view(request)
+
+        raw_conversion = json.loads(input_object)
+        expected_response_data = {
+            "data": data,
+            "raw_conversion": raw_conversion,
+            "prettified_conversion": pprint.pformat(raw_conversion),
+        }
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.data, dict)
+        self.assertEqual(response.data, expected_response_data)
