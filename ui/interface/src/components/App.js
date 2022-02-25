cbea511fae24509a79315255531b79c8a55a0753
Edchel Stephen <edchelstephens@gmail.com>
2022-02-25 12:57:14 +0800

    update app to call api on text change.

--- ui/interface/src/components/App.js
+++ ui/interface/src/components/App.js
@@ -1,9 +1,29 @@
+import { useState } from "react";
+
+import axios from "axios";
+
 const App = () => {
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("");
+
+  const handleInputChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const handleConvertClick = async (event) => {
+    event.preventDefault();
+    let data = {
+      data: input,
+    };
+    let response = await axios.post("/conversions/js-to-python/", data);
+    setOutput(response.data.prettified_conversion);
+  };
+
   return (
     <div className="app">
-      <input type="text"></input>
-      <button>Convert</button>
-      <input type="text"></input>
+      <input type="text" value={input} onChange={handleInputChange}></input>
+      <button onClick={handleConvertClick}>Convert</button>
+      <input type="text" value={output}></input>
     </div>
   );
 };
