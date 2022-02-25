316d816d08d6cc0429b060c510882d05673e168d
Edchel Stephen <edchelstephens@gmail.com>
2022-02-25 12:35:07 +0800

    pass first tes.

--- ui/interface/src/components/tests/App.test.js
+++ ui/interface/src/components/tests/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { shallow } from "enzyme";
+import App from "../App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  it("renders without crashing", () => {
+    let mountedApp = shallow(<App />);
+  });
 });
