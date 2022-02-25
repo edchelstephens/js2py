1ded1ac35694a792aac988b01892529d77cb4ca3
Edchel Stephen <edchelstephens@gmail.com>
2022-02-25 18:24:40 +0800

    update database configuration.

--- js2py/settings.py
+++ js2py/settings.py
@@ -75,14 +75,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "js2py.wsgi.application"
 
+from .local_settings import DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": DB_NAME,
+        "USER": DB_USER,
+        "PASSWORD": DB_PASSWORD,
+        "HOST": DB_HOST,
+        "PORT": DB_PORT,
     }
 }
 
